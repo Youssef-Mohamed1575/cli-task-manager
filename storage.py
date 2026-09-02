f4210d4a04e395709b647e760d1ac7b1dbc9229c
Youssef-Mohamed1575 <yoyo2005.yi@gmail.com>
2026-09-03 02:37:24 +0300

    refactor(storage): add username field layout

--- storage.py
+++ storage.py
@@ -2,16 +2,21 @@ import json
 import os
 
 FILE_PATH = "tasks.json"
-def load_tasks():
+default_struct = {
+    "username" : "",
+    "tasks" : []
+}
+
+def load_data():
     if not(os.path.exists(FILE_PATH)):
-        return[]
+        return default_struct
     try:
         with open(FILE_PATH,"r") as f :
             data = json.load(f)
             return data
     except json.JSONDecodeError:
-        return[]
+        return default_struct
 
-def save_tasks(tasks):
+def save_data(data):
         with open(FILE_PATH,"w") as f :
-            json.dump(tasks,f,indent=2)
+            json.dump(data,f,indent=2)
